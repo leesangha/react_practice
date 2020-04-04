49b50352e9cb1bd81571e3554dce3cfc2a98872e
leesangha <37141545+leesangha@users.noreply.github.com>
2020-04-04 16:28:23 +0900

    Create Research.js

---
+++ Research.js
@@ -0,0 +1,21 @@
+import React, { useCallback } from 'react';
+
+
+function Research() {
+   
+    return(
+        <div>
+            <ol>
+                <li>싫어하는 음식이 있나요?</li>
+
+                <li>못먹는 재료가 있나요?</li>
+                <input name ="search" placeholder="재료를 검색하세요" ></input>
+            </ol>
+
+        </div>
+    )
+    
+
+}
+
+export default Research;

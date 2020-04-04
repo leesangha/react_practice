de8560184cb6b96aceb0584aaead6762130a190d
leesangha <sanghalee1@naver.com>
2020-04-04 18:51:02 +0900

    Update Research.js
    업데이트를 진행합니다.

--- Research.js
+++ Research.js
@@ -3,13 +3,16 @@ import React, { useCallback } from 'react';
 
 function Research() {
    
+    const onChange = useCallback( e=>{
+
+    })
     return(
         <div>
             <ol>
                 <li>싫어하는 음식이 있나요?</li>
 
                 <li>못먹는 재료가 있나요?</li>
-                <input name ="search" placeholder="재료를 검색하세요" ></input>
+                <input name ="search" placeholder="재료를 검색하세요" onChange = {onChange} ></input>
             </ol>
 
         </div>
